c824fe9a51156b37c67d6c1d6efd0c80c11acccb
rishitdaru <rishitnewyork@gmail.com>
2025-03-15 18:17:38 -0400

    Initialized project

---
+++ src/main/java/com/mvc/secure_app/SecureAppApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.secure_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAppApplication.class, args);
+	}
+
+}
